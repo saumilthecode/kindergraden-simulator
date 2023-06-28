9a73e636e79692f898781b393cdad328e2098df9
potato dog man <saumil707@gmail.com>
2023-06-28 16:05:49 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("HOW COULD YOU")
+})
+basic.forever(function () {
+    basic.showString("saumil")
+    basic.pause(1000)
+    basic.showIcon(IconNames.Scissors)
+})
