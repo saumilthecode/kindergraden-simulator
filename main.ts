38ef1b136e0e000c26daebf12d4d7aeeb1667f57
potato dog man <saumil707@gmail.com>
2023-06-28 16:37:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
-    basic.showString("drunk person threatening me! call police")
-})
-input.onButtonPressed(Button.AB, function () {
-    music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.InBackground)
-    basic.showString("she fainted! call ambulance")
-})
-input.onButtonPressed(Button.B, function () {
-    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.InBackground)
-    basic.showString("fire fire! call firefighters")
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    number = randint(1, 6)
+    basic.showNumber(number)
 })
 basic.forever(function () {
 	
