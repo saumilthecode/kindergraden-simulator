f44cb43b0c2a6849f1dc879a490060cedc0eaba9
potato dog man <saumil707@gmail.com>
2023-06-28 16:41:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,55 @@
 let number = 0
 input.onGesture(Gesture.Shake, function () {
     number = randint(1, 6)
-    basic.showNumber(number)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (number == 5) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . # . # .
+            . . . . .
+            `)
+    }
 })
 basic.forever(function () {
 	
