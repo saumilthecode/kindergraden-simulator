d62ff9685bdbfe76cf9bab620d4314d204ea4051
potato dog man <saumil707@gmail.com>
2023-06-28 16:14:10 +0800

    god help me

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("HOW COULD YOU")
+    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+    basic.showString("drunk person threatening me! call police")
+})
+input.onButtonPressed(Button.AB, function () {
+    music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.InBackground)
+    basic.showString("she fainted! call ambulance")
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.InBackground)
+    basic.showString("fire fire! call firefighters")
 })
 basic.forever(function () {
-    basic.showString("saumil")
-    basic.pause(1000)
-    basic.showIcon(IconNames.Scissors)
+	
 })
